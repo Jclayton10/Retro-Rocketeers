2fe144081135089ddd8211d1c27dd5c624259ef2
Jclayton10 <jclayton10@email.davenport.edu>
2023-10-19 14:03:27 -0400

    Minimap
    Added a minimap

---
+++ Assets/MinimapCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinimapCamera : MonoBehaviour
+{
+    [SerializeField] Transform playerPos;
+    [SerializeField] RenderTexture rendTexture;
+    [SerializeField] RawImage minimapImage;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerPos.position.x, transform.position.y, playerPos.position.z);
+        minimapImage.texture = rendTexture;
+    }
+}
