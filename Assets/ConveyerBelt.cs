b489f6b5bf61f052c7f6577a1280c413663bced1
Jclayton10 <jclayton10@email.davenport.edu>
2023-10-26 15:51:31 -0400

    Conveyer Belt
    Working on Conveyer Belt

---
+++ Assets/ConveyerBelt.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyerBelt : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5;
+    [SerializeField] List<Rigidbody> objectsOnConveyer = new List<Rigidbody>();
+
+    private void Update()
+    {
+        foreach (Rigidbody rb in objectsOnConveyer)
+        {
+            rb.velocity = -transform.forward * moveSpeed;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Rigidbody>() != null)
+        {
+            objectsOnConveyer.Add(other.GetComponent<Rigidbody>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>() != null)
+        {
+            objectsOnConveyer.Remove(other.GetComponent<Rigidbody>());
+        }
+    }
+}
