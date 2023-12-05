ce0d7777f3b933e8ad230c3d45c3172f703f4f52
Miranda Morris <juniperistic@gmail.com>
2023-12-04 20:07:17 -0500

    Added in Gardening Building Model & Prefab

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,8 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public InventoryManagement inventory;
+
+    public static PlayerController playerController { get; private set; }
+
+    [Header("Player Varibles")]
+    public int healthRestored;
+    public int currentHealth = 100;
+    private int maxHealth;
+
+    [Header("Player HealthBar Varibles")]
+    public Slider playerHealthSlider;
 
     void Update()
     {
@@ -11,7 +24,22 @@ public class PlayerController : MonoBehaviour
             if (inventory.selectedItem != null)
             {
                 inventory.selectedItem.Use(this);
+
+                //Heal();
             }
         }
     }
+
+
+    public void SetHealthUI(int health)
+    {
+        playerHealthSlider.value = health;
+    }
+
+    //Healing the Player using Food
+    //public void Heal(int healthRestored)
+    //{
+    //    currentHealth += healthRestored;
+    //    SetHealthUI(currentHealth);
+    //}
 }
